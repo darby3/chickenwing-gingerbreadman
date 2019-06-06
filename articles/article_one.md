438f2fa5a03977b804bbf1da59bc2ac849f13fc0
Darby Dixon III <ddixon@oberlin.edu>
2019-06-06 17:20:24 -0400

    Making a hot mess in the search of basic blog functionality.

---
+++ articles/article_one.md
@@ -0,0 +1,13 @@
+---
+title: How much fun are we having?
+---
+
+# This is my cool article
+
+This is the content of my cool article
+
+## This is my subjead
+
+## Please render me to HTML
+
+> bling
--- assemblefile.js
+++ assemblefile.js
@@ -2,12 +2,54 @@
 
 var assemble = require('assemble');
 var extname = require("gulp-extname");
+var permalink = require('assemble-permalinks');
+var viewEvents = require('./plugins/view-events');
+var merge = require('mixin-deep');
+var path = require('path');
+
 var app = assemble();
+app.use(viewEvents('permalink'));
+app.use(permalink());
+
+app.onPermalink( /./, function(file, next) {
+  console.log("XYZHAH PERMANENT LINK ACTIVATED");
+
+  file.data = merge({}, app.cache.data, file.data);
+  next();
+});
 
 app.layouts('templates/layouts/**/*.hbs');
 app.partials('templates/partials/**/*.hbs');
 
 app.option('layout', 'main');
+
+app.create('articles');
+
+app.articles.option('layout', 'markdown');
+app.articles.use(permalink('articles/:name/index.html'));
+
+app.preRender( /./, function ( view, next ) {
+  view.data.articles = app.views.articles;
+
+  next();
+} );
+
+app.task( 'content:articles', function () {
+  app.helper( 'markdown', require( 'helper-markdown' ) );
+  app.articles('articles/**/*.{md,hbs}');
+
+  return app.toStream( 'articles' )
+    .pipe( app.renderFile() )
+    .pipe( extname() )
+    .pipe( app.dest(function(file) {
+
+      console.log(file.data);
+
+      file.path = file.data.permalink;
+      file.base = path.dirname(file.path);
+      return 'build/' + file.base;
+    }) );
+} );
 
 app.task('default', function() {
   app.pages('pages/**/*.hbs');
