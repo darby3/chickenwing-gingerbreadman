f996a4bf24b24982b3bf27ac075975e08b592533
Darby Dixon III <ddixon@oberlin.edu>
2019-10-25 14:22:38 -0400

    Moving to a permalinks set-up.

--- assemblefile.js
+++ assemblefile.js
@@ -11,6 +11,10 @@ var app = assemble();
 app.layouts('templates/layouts/**/*.hbs');
 app.partials('templates/partials/**/*.hbs');
 
+var paths = {
+  buildDir: path.join( __dirname, './build' ),
+};
+
 app.option('layout', 'main');
 
 app.create('updates');
@@ -48,25 +52,45 @@ app.task( 'listing:updates', ['content:updates'], function() {
 
   app.pages('listing/**/updates.hbs');
   return app.toStream('pages')
+    .pipe( app.permalink('updates/index.html') )
     .pipe(app.renderFile())
     .pipe(extname())
-    .pipe(app.dest('build'));
+    .pipe( app.dest(function(file) {
+      file.path = file.data.permalink;
+      file.base = path.dirname(file.path);
+      return 'build/' + file.base;
+    }) );
 });
 
-app.task('rebuild:all', ['listing:updates'], function() {
-  app.pages('pages/**/*.hbs');
+app.task( 'home', function() {
+  app.pages('home/index.hbs');
   return app.toStream('pages')
+    .pipe( app.permalink('index.html') )
     .pipe(app.renderFile())
     .pipe(extname())
-    .pipe(app.dest('build'));
+    .pipe( app.dest(function(file) {
+      file.path = file.data.permalink;
+      file.base = path.dirname(file.path);
+      return 'build/' + file.base;
+    }) );
 });
 
 app.task('default', function() {
   app.pages('pages/**/*.hbs');
   return app.toStream('pages')
+    .pipe( app.permalink( path.join( '.', ':getQualifiedName()/index.html' ), {
+      getQualifiedName: function () {
+        var relPath = path.relative( this.base, this.dirname );
+        return path.join( relPath, this.name );
+      }
+    }) )
     .pipe(app.renderFile())
     .pipe(extname())
-    .pipe(app.dest('build'));
+    .pipe( app.dest(function(file) {
+      file.path = file.data.permalink;
+      file.base = path.dirname(file.path);
+      return 'build/' + file.base;
+    }) );
 });
 
 module.exports = app;
