1b1480c0a4effb1d88ac9f0a19ef5744e9b52c0b
Darby Dixon III <ddixon@oberlin.edu>
2020-07-02 17:26:45 -0400

    Adding some animations.

--- js/toggle-a-class.js
+++ js/toggle-a-class.js
@@ -1,7 +1,7 @@
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector('#addClass');
-    const box = document.querySelector('.anim-box');
+    const box = document.querySelector('.simple-box');
 
     button.addEventListener('click', function() {
       box.classList.toggle('active');
