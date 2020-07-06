dc3183c5db0dc2d9f68cf5d46c97561c22094a22
Darby Dixon III <ddixon@oberlin.edu>
2020-07-06 13:06:48 -0400

    Slightly better, if also slower, getAllJs script.

--- getAllJs.sh
+++ getAllJs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "getAllJs"
+
 for entry in js/*.js
 do
-  npx browserify $entry -t babelify -o build/$entry
+  npx browserify $entry -t babelify -o tmp/$entry
 done
+
+mkdir -p build/js
+mv tmp/js/* build/js/
