44b302838b8b8affaef6b130df62392411f143fd
Darby Dixon III <ddixon@oberlin.edu>
2023-01-31 16:49:43 -0500

    JS clean.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,13 @@
 // main scripts
 (function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    // about link
+    const aboutLink = document.getElementById("aboutLink");
+    const aboutDialog = document.getElementById("about");
 
-    document.addEventListener('DOMContentLoaded', function () {
-
-        // Let's get this party started.
-        console.log("Hello from the main app file");
-
-        // Requires and module initializations
-        var hiThere = require("./modules/helloThere");
-        hiThere();
-
-        // uhm.js
-        const y = 9;
-        var x = a => 1 + 3 + a;
-        var z = x(y);
-        var q = x(25);
-
-        console.log('z = ', z);
-        console.log('q = ', q);
-
-
-        // about link
-        const aboutLink = document.getElementById('aboutLink');
-        const aboutDialog = document.getElementById('about');
-
-        aboutLink.addEventListener('click', (e) => {
-            e.preventDefault();
-            aboutDialog.showModal();
-        });
-
-
+    aboutLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      aboutDialog.showModal();
     });
-
-}());
+  });
+})();
